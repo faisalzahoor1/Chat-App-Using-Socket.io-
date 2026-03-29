e6c1a0ba22b35c53e4c0a8259f23619616a8be56
M. Faisal Zahoor <faisalzahoor96@gmail.com>
2026-03-30 01:20:10 +0500

    state recovery

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const __dirname = dirname(__filename);
 const app = express()
 const server = http.createServer(app)
 
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3'
+
+const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+});
+await db.exec(`
+    CREATE TABLE If NOT EXISTS messages(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT);
+    `
+);
 const io = new Server(server, {
     connectionStateRecovery: {}
 })
@@ -21,13 +34,29 @@ app.get("/", (req, res) => {
     return res.sendFile(join(__dirname, "index.html"))
 })
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        const messageWithId = `${socket.id}: ${msg}`;
-        messages.push(messageWithId);
-        io.emit('chat message', messageWithId);
+io.on('connection', async(socket) => {
+    socket.on('chat message', async(msg) => {
+        let result;
+        try {
+            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+        io.emit('chat message', msg, result.lastID)
+        // const messageWithId = `${socket.id}: ${msg}`;
+        // messages.push(messageWithId);
+        // io.emit('chat message', messageWithId);
     });
-
+    if (!socket.recovered) {
+        try {
+            await db.each('SELECT id, content FROM messages WHERE id > ?', [socket.handshake.auth.serverOffset || 0], (_err, row)=>{
+                socket.emit('chat message', row.content, row.id)
+            })
+        } catch (error) {
+            return;
+        }
+    }
 
 });
 
